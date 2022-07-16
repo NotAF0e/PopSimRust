af6faf2fec533f8a1004d470de6949dc2d4b5b4f
Phonixed456 <pawel.zdunowski123@gmail.com>
2022-07-16 10:00:42 +0100

    bruh main is not a test(Removed test text at top of file)

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-# This is a test version of the game to add larger experimental features
-# These features could break the game when added incorrectly, so I will code them in here instead
-
 import random
 import time
 # import json
