650363617b5f5d2dcddb66253f659277a7a98c01
Phonixed456 <pawel.zdunowski123@gmail.com>
2022-09-01 16:54:30 +0100

    Added some debug stuff and files of pop-sim are now in a new directory

--- pop-sim/pop-sim.py
+++ pop-sim/pop-sim.py
@@ -1,5 +1,5 @@
 import random
-
+import timeit
 
 male_names = open("male-names.txt").readlines()
 female_names = open("female-names.txt").readlines()
@@ -12,6 +12,17 @@ def giveName():
     both_gender_names = [random.choice(male_names).replace("\n", ""),
                          random.choice(female_names).replace("\n", "")]
     return both_gender_names
+
+
+def debugTimer(mode):
+    _debug_start = 0
+    _debug_end = 0
+    if mode == "s":
+        _debug_start = timeit.timeit()
+    elif mode == "e":
+        _debug_end = timeit.timeit()
+        _time = _debug_end - _debug_start
+        print(_time)
 
 
 class Sim:
@@ -40,12 +51,10 @@ class Sim:
         self.temp_person.append(gender)
 
         # Gives person a starting affection rating
-        # self.people.append()
+        self.temp_person.append(50)
 
         # Appends the person to people
         self.people.append(self.temp_person)
-
-
 
     def printPeople(self):
         gender = ["Male", "Female"]
@@ -57,20 +66,24 @@ class Sim:
                   f"Age: {age_years} years, {age_months} months\n"
                   f"Gender: {gender[self.p[2]]}\n")
 
+    # def passTime(self, amount_of_time):
 
-    def passTime(self, amount_of_time):
+    def updateSim(self, amount_of_time):
         if amount_of_time == "": amount_of_time = 1
         amount_of_time = int(amount_of_time)
-        for self.p in self.people:
-            self.p[1] += amount_of_time
-            print(self.p[1])
+
+        for self.time in range(amount_of_time):
+            # Adds age to all people
+            for self.p in self.people:
+                self.p[1] += amount_of_time
 
 
-    # def updateAffection(self):
-    #     for
+
 Sim.createPerson(Sim(), giveName())
 Sim.createPerson(Sim(), giveName())
 while True:
     Sim.printPeople(Sim())
     time_amount = input(">>> ").strip()
-    Sim.passTime(Sim(), time_amount)
+    debugTimer("s")
+    Sim.updateSim(Sim(), time_amount)
+    debugTimer("e")
