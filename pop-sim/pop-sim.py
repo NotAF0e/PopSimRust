361ca25248780c0741c99bd0abc5c2d3dc2d2397
Phonixed456 <pawel.zdunowski123@gmail.com>
2022-09-12 18:26:15 +0100

    optimised main lover chooser code, added rich track support so pip install "rich", fixed issues with graph, added more code comments

--- pop-sim/pop-sim.py
+++ pop-sim/pop-sim.py
@@ -1,6 +1,8 @@
 import random
 import time
 import matplotlib.pyplot as plt
+from rich.progress import track
+
 
 male_names = open("male-names.txt").readlines()
 female_names = open("female-names.txt").readlines()
@@ -29,6 +31,7 @@ class Sim:
         self.love_lst = None
         self.p = None
         self.temp_person = None
+
 
     def createPerson(self):
         global population
@@ -77,23 +80,22 @@ class Sim:
                   f"Age: {age_years} years, {age_months} months\n"
                   f"Gender: {gender[self.p[3]]}\n")
 
-    # def passTime(self, amount_of_time):
-
     def updateSim(self, amount_of_time):
         global months_passed
+        debugTimer("s")
+
         if amount_of_time == "": amount_of_time = 1
         amount_of_time = int(amount_of_time)
         months_passed += amount_of_time
 
-        for self.time in range(amount_of_time):
-
+        for self.time in track(range(amount_of_time), "Total load\n"):
             # Adds age to all people or kills them
             for self.p in self.people:
                 self.p[2] += 1
                 if self.p[2] > 35*12:
                     temp_person = self.people.remove(self.p)
                     self.dead_people.append(temp_person)
-                    del self.people[self.p[0]]
+                    # del self.people[self.p[0]]
 
             # Calculates who will reproduce
             for self.p in self.people:
@@ -101,25 +103,36 @@ class Sim:
                 # Chooses lover unless person already has one
                 if self.p[2] > 15*12 and not self.p[4][1]:
                     if not self.p[4][0]:
-                        choice_of_lover = None
+                        choices_of_lovers = []
                         # print(self.people)
-                        while choice_of_lover is None or choice_of_lover == self.p[0]:
-                            choice_of_lover = random.choice(self.people)[0]
-                            # print(choice_of_lover)
+                        for temp_lover in self.people:
+                            if temp_lover != self.p[0]:
+                                choices_of_lovers.append(temp_lover[0])
+                                # print(choices_of_lovers)
 
-                        if len(self.people) > choice_of_lover and self.people[choice_of_lover][2] > 15*12:
+                        choice_of_lover = None
+
+                        # Chooses random lover from list of applicable
+                        if choices_of_lovers:
+                            for x in range(10):
+                                choice_of_lover = random.choice(choices_of_lovers)
+
+                        if choice_of_lover and choice_of_lover < len(self.people) and self.people[choice_of_lover][2] > 15*12:
                             self.p[4][0] = choice_of_lover
 
                     else:
+                        # Sets lover
                         for self.temp_person in self.people:
                             if self.p[4][0] == self.temp_person[0] and random.randint(0, 100) < 10:
                                 self.temp_person[4][1] = True
                                 self.p[4][1] = True
 
+                # Checks if baby should be born
                 for self.temp_person in self.people:
                     if self.p[4][0] == self.temp_person[0] and self.p[4][1] and random.randint(0, 100) < 8:
                         # Creates a baby!!!
                         self.createPerson()
+
 
 
 Sim.createPerson(Sim())
@@ -139,24 +152,21 @@ while True:
             pop.append(p)
 
         pop.reverse()
-        print(pop, mp)
+        print(mp, pop)
         fig, ax = plt.subplots()
-        ax.plot(pop, mp)
+        ax.plot(mp, pop)
 
         ax.set(xlabel='time (months)', ylabel='population',
                title='Pop-sim population/time graph')
         ax.grid()
 
-        fig.savefig("test.png")
         plt.show()
         time_amount = 0
     elif time_amount == "p":
         print(f"Population: {len(Sim.people)}\n")
         time_amount = 0
-    debugTimer("s")
+
     Sim.updateSim(Sim(), time_amount)
     Sim.printPeople(Sim())
     debugTimer("e")
-
-
     # print(Sim.people)
