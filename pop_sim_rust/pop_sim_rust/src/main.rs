e923abff793de58933f9a9d72f4de8d8c1a323eb
Phonixed456 <pawel.zdunowski123@gmail.com>
2022-09-27 18:11:05 +0100

    full rewrite to rust???

---
+++ pop_sim_rust/pop_sim_rust/src/main.rs
@@ -0,0 +1,12 @@
+// let population = -1;
+
+fn main() {
+    let mut population: i64 = -1;
+    let _months_passed = 0;
+
+    while population != 100{
+        println!("{}", population);
+        population += 1
+    }
+    
+}
---
+++ pop_sim_rust/pop_sim_rust/target/debug/deps/pop_sim_rust-59069dadc269f530.d
@@ -0,0 +1,5 @@
+Z:\.PYTHON_PROJECTS\SIMULATIONS\pop_sim_rust\pop_sim_rust\target\debug\deps\pop_sim_rust-59069dadc269f530.rmeta: src\main.rs
+
+Z:\.PYTHON_PROJECTS\SIMULATIONS\pop_sim_rust\pop_sim_rust\target\debug\deps\pop_sim_rust-59069dadc269f530.d: src\main.rs
+
+src\main.rs:
