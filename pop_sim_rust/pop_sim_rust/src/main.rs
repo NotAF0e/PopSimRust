9b86e1778185ff037c4f49980b2399e3e9515d23
Phonixed456 <pawel.zdunowski123@gmail.com>
2022-09-28 19:14:39 +0100

    began rust rewrite(I don't know what im doing lol)

--- pop_sim_rust/pop_sim_rust/src/main.rs
+++ pop_sim_rust/pop_sim_rust/src/main.rs
@@ -1,12 +1,20 @@
-// let population = -1;
+// use std::time::{Duration, Instant};
+
+fn update_sim(){
+    let _months_passed = 0;
+    pub const POPULATION: i64 = -1;
+    pub struct Sim {
+    people: Vec<i32>,
+    dead_people: Vec<i32>
+    }
+}
 
 fn main() {
-    let mut population: i64 = -1;
-    let _months_passed = 0;
 
-    while population != 100{
-        println!("{}", population);
-        population += 1
-    }
-    
+    // Graphing variables
+    let mp: Vec<i32> = Vec::new();
+    let pop: Vec<i32> = Vec::new();
+    let tp: i32 = -1;
+
+
 }
