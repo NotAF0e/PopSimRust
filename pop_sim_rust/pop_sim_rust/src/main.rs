224a12a3caebbbf8fc7d55e4656534fff1c606b1
Phonixed456 <pawel.zdunowski123@gmail.com>
2022-09-29 14:50:25 +0100

    more rust rewrite stuff(I don't know what im doing lol)

--- pop_sim_rust/pop_sim_rust/src/main.rs
+++ pop_sim_rust/pop_sim_rust/src/main.rs
@@ -1,20 +1,33 @@
-// use std::time::{Duration, Instant};
+use std::str;
 
-fn update_sim(){
-    let _months_passed = 0;
-    pub const POPULATION: i64 = -1;
-    pub struct Sim {
-    people: Vec<i32>,
-    dead_people: Vec<i32>
-    }
+#[derive(Debug)]
+pub struct Person{
+    name: &'static str,
+    age: i32,
+    gender: i8
+}
+
+
+fn create_person() -> Person {
+    let temp_person = Person{name: "John", age: 0, gender: 0};
+
+    return temp_person
 }
 
 fn main() {
+    let mut people: Vec<Person> = Vec::new();
+    let john: Person = create_person();
+
+    people.push(john);
 
     // Graphing variables
-    let mp: Vec<i32> = Vec::new();
-    let pop: Vec<i32> = Vec::new();
-    let tp: i32 = -1;
+    // let mp: Vec<i32> = Vec::new();
+    // let pop: Vec<i32> = Vec::new();
+    // let tp: i32 = -1;
 
+
+    println!("Name: {:?}\n\
+             Age: {:?}\n\
+             Gender: {:?}\n", people[0].name, people[0].age, people[0].gender)
 
 }
