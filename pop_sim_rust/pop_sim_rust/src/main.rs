338751836b492fc987d179a574800ee2201a84a9
Phonixed456 <pawel.zdunowski123@gmail.com>
2022-10-10 16:02:03 +0100

    removed realistic date for parity with python ver(Also i didn't get it, lol), Some formatting too

--- pop_sim_rust/pop_sim_rust/src/main.rs
+++ pop_sim_rust/pop_sim_rust/src/main.rs
@@ -1,56 +1,56 @@
 use std::str;
-use gregorian::{Date};
 
 // Person data struct
 #[derive(Debug)]
 pub struct Person {
     id: i32,
     name: &'static str,
-    age: f32,
+    age: i32,
     gender: i16,
 }
 
 fn main() {
-    let date: Date = Date::new(0000, 1, 1).unwrap();
     static mut POPULATION: i32 = -1;
     static mut PEOPLE: Vec<Person> = Vec::new();
 
     pub unsafe fn create_person() -> Person {
         POPULATION += 1;
-        let temp_person: Person = Person { name: "John", age: 0.0, gender: 0, id: POPULATION };
+        let temp_person: Person = Person {
+            name: "John",
+            age: 0,
+            gender: 0,
+            id: POPULATION,
+        };
 
         return temp_person;
     }
-    let add_day = ||{
-        date.add_days(1);
-        println!("{:?}", date)
-    };
 
     pub fn update_sim() {
         let people_temp = unsafe { &mut PEOPLE };
-        for id in 0..2 {
-            people_temp[id].age += 1.0;
+        for id in 0..unsafe { PEOPLE.len() } {
+            people_temp[id].age += 1;
         }
     }
 
     pub fn print_people() {
         for id in 0..unsafe { PEOPLE.len() } {
+            println!("-------------------------------------------");
             unsafe {
                 println!("[ID: {:?}]\n\
                   Name: {:?}\n\
                   Age: {:?}\n\
-                  Gender: {:?}\n", PEOPLE[id].id, PEOPLE[id].name, (PEOPLE[id].age / 12.0),
+                  Gender: {:?}", PEOPLE[id].id, PEOPLE[id].name, PEOPLE[id].age,
                          PEOPLE[0].gender)
             }
         }
     }
-    let people_vec = unsafe { &mut PEOPLE };
+    let people_temp = unsafe { &mut PEOPLE };
 
     let john: Person = unsafe { create_person() };
-    people_vec.push(john);
+    people_temp.push(john);
 
     let john2: Person = unsafe { create_person() };
-    people_vec.push(john2);
+    people_temp.push(john2);
 
     // Graphing variables
     // let mp: Vec<i32> = Vec::new();
@@ -58,7 +58,7 @@ fn main() {
     // let tp: i32 = -1;
 
     print_people();
-    add_day();
+    update_sim();
     update_sim();
     print_people();
 }
