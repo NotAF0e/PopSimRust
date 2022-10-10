7d9d1bacaf3b59ddbf2264fdaa94a692a69182dd
Phonixed456 <pawel.zdunowski123@gmail.com>
2022-10-10 18:49:08 +0100

    people can now love each over

--- pop_sim_rust/pop_sim_rust/src/main.rs
+++ pop_sim_rust/pop_sim_rust/src/main.rs
@@ -1,12 +1,14 @@
 use std::str;
+use rand::Rng;
 
 // Person data struct
 #[derive(Debug)]
 pub struct Person {
     id: i32,
     name: &'static str,
-    age: i32,
     gender: i16,
+    age: i32,
+    love_vec: Vec<i32>,
 }
 
 fn main() {
@@ -16,10 +18,11 @@ fn main() {
     pub unsafe fn create_person() -> Person {
         POPULATION += 1;
         let temp_person: Person = Person {
-            name: "John",
-            age: 0,
-            gender: 0,
             id: POPULATION,
+            name: "John",
+            gender: 0,
+            age: 0,
+            love_vec: vec![-1, 100],
         };
 
         return temp_person;
@@ -28,7 +31,17 @@ fn main() {
     pub fn update_sim() {
         let people_temp = unsafe { &mut PEOPLE };
         for id in 0..unsafe { PEOPLE.len() } {
+            // Ages all people by 1 day
             people_temp[id].age += 1;
+
+            // Creates a random number to chose a lover for person
+            let lover = rand::thread_rng().gen_range(0..=(unsafe { PEOPLE.len() } - 1)) as i32;
+
+            // If the person is not the lover and if the person does not have a lover one is given
+            if lover != id as i32 && people_temp[id].love_vec[0] == -1 {
+                println!("{} from {}", &lover, &id);
+                people_temp[id].love_vec[0] = lover;
+            }
         }
     }
 
@@ -39,8 +52,9 @@ fn main() {
                 println!("[ID: {:?}]\n\
                   Name: {:?}\n\
                   Age: {:?}\n\
-                  Gender: {:?}", PEOPLE[id].id, PEOPLE[id].name, PEOPLE[id].age,
-                         PEOPLE[0].gender)
+                  Gender: {:?}\n\
+                  Lover: {:?}", PEOPLE[id].id, PEOPLE[id].name, PEOPLE[id].age,
+                         PEOPLE[id].gender, PEOPLE[id].love_vec)
             }
         }
     }
