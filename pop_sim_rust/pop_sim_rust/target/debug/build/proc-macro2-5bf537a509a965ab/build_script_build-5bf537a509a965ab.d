a7a0c10751c785df094ac58dbb413f5f8e6bc14f
Phonixed456 <pawel.zdunowski123@gmail.com>
2022-10-18 19:28:30 +0100

    trying to speed it up more

---
+++ pop_sim_rust/pop_sim_rust/target/debug/build/proc-macro2-5bf537a509a965ab/build_script_build-5bf537a509a965ab.d
@@ -0,0 +1,5 @@
+Z:\.PYTHON_PROJECTS\SIMULATIONS\pop_sim_rust\pop_sim_rust\target\debug\build\proc-macro2-5bf537a509a965ab\build_script_build-5bf537a509a965ab.exe: C:\Users\Phonixed\.cargo\registry\src\github.com-1ecc6299db9ec823\proc-macro2-1.0.47\build.rs
+
+Z:\.PYTHON_PROJECTS\SIMULATIONS\pop_sim_rust\pop_sim_rust\target\debug\build\proc-macro2-5bf537a509a965ab\build_script_build-5bf537a509a965ab.d: C:\Users\Phonixed\.cargo\registry\src\github.com-1ecc6299db9ec823\proc-macro2-1.0.47\build.rs
+
+C:\Users\Phonixed\.cargo\registry\src\github.com-1ecc6299db9ec823\proc-macro2-1.0.47\build.rs:
---
+++ pop_sim_rust/pop_sim_rust/target/debug/deps/lazy_static-59a9d36f4fb6c529.d
@@ -0,0 +1,6 @@
+Z:\.PYTHON_PROJECTS\SIMULATIONS\pop_sim_rust\pop_sim_rust\target\debug\deps\lazy_static-59a9d36f4fb6c529.rmeta: C:\Users\Phonixed\.cargo\registry\src\github.com-1ecc6299db9ec823\lazy_static-1.4.0\src\lib.rs C:\Users\Phonixed\.cargo\registry\src\github.com-1ecc6299db9ec823\lazy_static-1.4.0\src\inline_lazy.rs
+
+Z:\.PYTHON_PROJECTS\SIMULATIONS\pop_sim_rust\pop_sim_rust\target\debug\deps\lazy_static-59a9d36f4fb6c529.d: C:\Users\Phonixed\.cargo\registry\src\github.com-1ecc6299db9ec823\lazy_static-1.4.0\src\lib.rs C:\Users\Phonixed\.cargo\registry\src\github.com-1ecc6299db9ec823\lazy_static-1.4.0\src\inline_lazy.rs
+
+C:\Users\Phonixed\.cargo\registry\src\github.com-1ecc6299db9ec823\lazy_static-1.4.0\src\lib.rs:
+C:\Users\Phonixed\.cargo\registry\src\github.com-1ecc6299db9ec823\lazy_static-1.4.0\src\inline_lazy.rs:
