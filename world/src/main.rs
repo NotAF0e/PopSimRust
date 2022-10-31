3c95b4b5214f68b5b54529c58c96a9d76f214099
Phonixed456 <pawel.zdunowski123@gmail.com>
2022-10-31 18:44:33 +0000

    tiles work, kinda(crash when out of bounds of arr)

--- world/src/main.rs
+++ world/src/main.rs
@@ -5,14 +5,34 @@ pub struct TileWorld {
     width: usize,
 }
 
-impl TileWorld {}
-
 fn main() {
     const WORLD: TileWorld = TileWorld {
         height: 10, //Change
         width: 15, //Change
     };
+
     let mut tiles = [[0; WORLD.width]; WORLD.height];
+
+    fn available_tiles(tiles_arr: [[i32; WORLD.width]; WORLD.height], pointer: Vec<usize>) -> Vec<Vec<usize>> {
+        let mut available: Vec<Vec<usize>> = vec![];
+        if tiles_arr[pointer[0]][pointer[1] - 1] == 0 {
+            available.push(vec![pointer[0], pointer[1] - 1]);
+            println!("left");
+        }
+        if tiles_arr[pointer[0]][pointer[1] + 1] == 0 {
+            available.push(vec![pointer[0], pointer[1] + 1]);
+            println!("right");
+        }
+        if tiles_arr[pointer[0] - 1][pointer[1]] == 0 {
+            available.push(vec![pointer[0] - 1, pointer[1]]);
+            println!("up");
+        }
+        if tiles_arr[pointer[0] + 1][pointer[1]] == 0 {
+            available.push(vec![pointer[0] + 1, pointer[1]]);
+            println!("down");
+        }
+        return available;
+    }
 
     let start = Instant::now();
 
@@ -20,6 +40,8 @@ fn main() {
     for tile in tiles {
         println!("{:?}", tile);
     }
+    let test = vec![4, 14];
+    println!("{:?}", available_tiles(tiles, test));
 
     println!("{:?}", start.elapsed());
 }
