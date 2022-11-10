e22aca2f4e44e6d5e226bffb6e89c52dbceebb8b
Phonixed456 <pawel.zdunowski123@gmail.com>
2022-11-10 18:45:49 +0000

    bruh

--- world/src/main.rs
+++ world/src/main.rs
@@ -1,55 +1,57 @@
-use std::time::Instant;
-
-pub struct TileWorld {
-    height: usize,
-    width: usize,
-}
-
-fn main() {
-    const WORLD: TileWorld = TileWorld {
-        height: 10, //Change
-        width: 15, //Change
-    };
-
-    let mut tiles = [[0; WORLD.width]; WORLD.height];
-
-    fn available_tiles(tiles_arr: [[i32; WORLD.width]; WORLD.height], pointer: Vec<usize>) -> Vec<Vec<usize>> {
-        let mut available: Vec<Vec<usize>> = vec![];
-        if tiles_arr[pointer[0]][pointer[1] - 1] == 0 {
-            available.push(vec![pointer[0], pointer[1] - 1]);
-            println!("left");
-        }
-        if tiles_arr[pointer[0]][pointer[1] + 1] == 0 {
-            available.push(vec![pointer[0], pointer[1] + 1]);
-            println!("right");
-        }
-        if tiles_arr[pointer[0] - 1][pointer[1]] == 0 {
-            available.push(vec![pointer[0] - 1, pointer[1]]);
-            println!("up");
-        }
-        if tiles_arr[pointer[0] + 1][pointer[1]] == 0 {
-            available.push(vec![pointer[0] + 1, pointer[1]]);
-            println!("down");
-        }
-        return available;
-    }
-
-    let start = Instant::now();
-
-    tiles[0][0] = 1;
-    for tile in tiles {
-        println!("{:?}", tile);
-    }
-    let test_pointer = vec![4, 13];
-    let available_tiles = available_tiles(tiles, test_pointer);
-
-    for tile in &available_tiles {
-        tiles[tile[0]][tile[1]] = 1;
-    }
-    println!("{:?}", available_tiles);
-
-    for tile in tiles {
-        println!("{:?}", tile);
-    }
-    println!("{:?}", start.elapsed());
-}
+//
+// use std::time::Instant;
+//
+// pub struct TileWorld {
+//     height: usize,
+//     width: usize,
+// }
+//
+// fn main() {
+//     const WORLD: TileWorld = TileWorld {
+//         height: 10, // Change
+//         width: 15, // Change
+//     };
+//
+//     let mut tiles = [[0; WORLD.width]; WORLD.height];
+//
+//     fn available_tiles(tiles_arr: [[i32; WORLD.width]; WORLD.height], pointer: Vec<usize>) -> Vec<Vec<usize>> {
+//         let mut available: Vec<Vec<usize>> = vec![];
+//
+//         if tiles_arr[pointer[0]][pointer[1] - 1] == 0 {
+//             available.push(vec![pointer[0], pointer[1] - 1]);
+//             println!("left");
+//         }
+//         if tiles_arr[pointer[0]][pointer[1] + 1] == 0 {
+//             available.push(vec![pointer[0], pointer[1] + 1]);
+//             println!("right");
+//         }
+//         if tiles_arr[pointer[0] - 1][pointer[1]] == 0 {
+//             available.push(vec![pointer[0] - 1, pointer[1]]);
+//             println!("up");
+//         }
+//         if tiles_arr[pointer[0] + 1][pointer[1]] == 0 {
+//             available.push(vec![pointer[0] + 1, pointer[1]]);
+//             println!("down");
+//         }
+//         return available;
+//     }
+//
+//     let start = Instant::now();
+//
+//     tiles[0][0] = 1;
+//     for tile in tiles {
+//         println!("{:?}", tile);
+//     }
+//     let test_pointer = vec![4, 0];
+//     let available_tiles = available_tiles(tiles, test_pointer);
+//
+//     for tile in &available_tiles {
+//         tiles[tile[0]][tile[1]] = 1;
+//     }
+//     println!("{:?}", available_tiles);
+//
+//     for tile in tiles {
+//         println!("{:?}", tile);
+//     }
+//     println!("{:?}", start.elapsed());
+// }
