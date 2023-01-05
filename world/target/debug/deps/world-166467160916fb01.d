8e9aceed552c846bb4b5edd29fe165f9cd22b8ff
NotAFoe <75176016+NotAF0e@users.noreply.github.com>
2023-01-05 19:43:29 +0000

    Delete world/target directory

--- world/target/debug/deps/world-166467160916fb01.d
+++
@@ -1,5 +0,0 @@
-Z:\.PYTHON_PROJECTS\SIMULATIONS\world\target\debug\deps\world-166467160916fb01.rmeta: src\main.rs
-
-Z:\.PYTHON_PROJECTS\SIMULATIONS\world\target\debug\deps\world-166467160916fb01.d: src\main.rs
-
-src\main.rs:
